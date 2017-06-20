6439db538e03784e678469b5beba3cbc7c18af36
danny <danny2mar91@gmail.com>
2017-06-20 13:27:33 +0530

    added

---
+++ folder/src/main/java/com/folder/test/App.java
@@ -0,0 +1,13 @@
+package com.folder.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
